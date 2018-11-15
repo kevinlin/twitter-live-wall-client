919bec03466054b75fe9bdaf5c61c44c9e829f1e
Kevin Lin <kevin.lin@zuhlke.com>
2018-11-15 23:31:44 +0800

    Move HomeComponent to src/components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
 import {ToolbarComponent} from './components/toolbar/toolbar.component';
 
 import {AppRoutingModule} from './modules/app-routing.module';
 import {TweetModule} from './modules/tweet/tweet.module';
 import {CommonService} from './services/common.service';
-import {HomeComponent} from './views/home/home.component';
 
 @NgModule({
   declarations: [
